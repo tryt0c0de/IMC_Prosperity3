18b05c65a635fffa6c6885d01bf015b253008035
tryt0c0de <miquelfd@innovation.es>
2025-03-29 15:00:31 -0400

    testing

--- Tutorial/trader.py
+++ Tutorial/trader.py
@@ -12,7 +12,7 @@ class Trader:
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            acceptable_price = 10;  # Participant should calculate this value
+            acceptable_price = 100;  # Participant should calculate this value
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
     

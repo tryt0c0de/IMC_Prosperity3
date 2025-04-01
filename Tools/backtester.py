c82cce2b0554743e67fc5b00c2a91324059ca792
tryt0c0de <miquelfd@innovation.es>
2025-03-31 23:20:21 -0400

    Understanding the trading logic

---
+++ Tools/backtester.py
@@ -0,0 +1,2 @@
+#Backtester for the IMC Trading Strategy Replicating the competition by reading the log_file
+
--- Tutorial/trader.py
+++ Tutorial/trader.py
@@ -12,10 +12,13 @@ class Trader:
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
-            acceptable_price = 100;  # Participant should calculate this value
             print("Acceptable price : " + str(acceptable_price))
             print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
-    
+            if product == "RAINFOREST_RESIN":
+                acceptable_price = 0
+            elif product == "KELP":
+                acceptable_price = 2015
+
             if len(order_depth.sell_orders) != 0:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 if int(best_ask) < acceptable_price:

d74b6d969ccd20d6d49a25662bb30aaa2c2ee9cd
tryt0c0de <miquelfd@innovation.es>
2025-04-03 01:09:22 -0400

    Trying to backtest my life

--- Tutorial/trader.py
+++ Tutorial/trader.py
@@ -6,14 +6,17 @@ class Trader:
     
     def run(self, state: TradingState):
         # Only method required. It takes all buy and sell orders for all symbols as an input, and outputs a list of orders to be sent
-        print("traderData: " + state.traderData)
-        print("Observations: " + str(state.observations))
+        # print("traderData: " + state.traderData)
+        # print("Observations: " + str(state.observations))
         result = {}
+        max_holdings = {"RAINFOREST_RESIN": 50, "KELP": 50}
+        current_holdings = {product: 0 for product in max_holdings.keys()}
+
         for product in state.order_depths:
             order_depth: OrderDepth = state.order_depths[product]
             orders: List[Order] = []
             #print("Acceptable price : " + str(acceptable_price))
-            print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
+            # print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(len(order_depth.sell_orders)))
             if product == "RAINFOREST_RESIN":
                 acceptable_price = 0
             elif product == "KELP":
@@ -22,14 +25,26 @@ class Trader:
             if len(order_depth.sell_orders) != 0:
                 best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
                 if int(best_ask) < acceptable_price:
-                    print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-    
+                    # print("BUY", str(-best_ask_amount) + "x", best_ask)
+                    available_holdings = max_holdings[product] - current_holdings[product]
+                    if available_holdings > 0:
+                        orders.append(Order(product, best_ask, best_ask_amount))
+                        current_holdings[product] += best_ask_amount
+                    else:
+                        orders.append(Order(product, best_ask, available_holdings))
+                        current_holdings[product] += available_holdings
+
             if len(order_depth.buy_orders) != 0:
                 best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
                 if int(best_bid) > acceptable_price:
-                    print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
+                    # print("SELL", str(best_bid_amount) + "x", best_bid)
+                    available_holdings = max_holdings[product] - current_holdings[product]
+                    if available_holdings > 0:
+                        orders.append(Order(product, best_bid, -best_bid_amount))
+                        current_holdings[product] -= best_bid_amount
+                    else:
+                        orders.append(Order(product, best_bid, -available_holdings))
+                        current_holdings[product] -= available_holdings
             
             result[product] = orders
     
